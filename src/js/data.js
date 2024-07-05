0434b2588249e559509d1dc927194cabca81bf28
Cheeshcake <mohamedazizhadjhassen@gmail.com>
2024-07-06 00:15:50 +0100

    services section done

---
+++ src/js/data.js
@@ -0,0 +1,44 @@
+export const services = [
+  {
+    image: "/assets/images/service1.png",
+    title: "Search engine",
+    color: "var(--green)",
+    bgcolor: "var(--grey)",
+    learncolor: "var(--black)"
+  },
+  {
+    image: "/assets/images/service2.png",
+    title: "Pay-per-click",
+    color: "var(--white)",
+    bgcolor: "var(--green)",
+    learncolor: "var(--black)"
+  },
+  {
+    image: "/assets/images/service3.png",
+    title: "Social media",
+    color: "var(--white)",
+    bgcolor: "var(--dark)",
+    learncolor: "var(--white)"
+  },
+  {
+    image: "/assets/images/service4.png",
+    title: "Email",
+    color: "var(--green)",
+    bgcolor: "var(--grey)",
+    learncolor: "var(--black)"
+  },
+  {
+    image: "/assets/images/service5.png",
+    title: "Content Creation",
+    color: "var(--white)",
+    bgcolor: "var(--green)",
+    learncolor: "var(--black)"
+  },
+  {
+    image: "/assets/images/service6.png",
+    title: "Analytics",
+    color: "var(--green)",
+    bgcolor: "var(--dark)",
+    learncolor: "var(--white)"
+  },
+]
