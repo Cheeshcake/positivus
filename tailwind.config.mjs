ea499584f2ff4dc54bccbbe97fd996d24677a20a
Cheeshcake <mohamedazizhadjhassen@gmail.com>
2024-07-05 20:36:11 +0100

    Setting up new astro project
    installed astro and integrated tailwindcss, added the variables in tailwind-config and the typography properties I will need for this project in global.css

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors:{
+			green: '#B9FF66',
+			black: '#000000',
+			white: '#FFFFFF',
+			grey: '#F3F3F3',
+		},
+		extend: {
+			colors:{
+				"dark": '#191A23',
+			}
+		},
+	},
+	plugins: [],
+}
