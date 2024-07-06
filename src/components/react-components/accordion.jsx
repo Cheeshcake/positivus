944b80442f71b754f8b91db312ad2cb8c849b23d
Cheeshcake <mohamedazizhadjhassen@gmail.com>
2024-07-07 00:31:20 +0100

    working process section done
    I integrated react to work on the list of accordions in this section

---
+++ src/components/react-components/accordion.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+
+const Accordion = ({ title, content, index }) => {
+  const [accordionOpen, setAccordionOpen] = useState(false);
+
+  return (
+    <div
+      className={`card p-4 md:p-12 rounded-3xl my-10 ${
+        accordionOpen ? "bg-green" : "bg-grey"
+      }`}
+    >
+      <div className="flex justify-between items-center w-full">
+        <div className="flex justify-between items-center">
+          <h1 className="acc-index">{index}</h1>
+          <h3 className="acc-title md:ml-7 pl-10 md:pl-0">{title}</h3>
+        </div>
+        <button onClick={() => setAccordionOpen(!accordionOpen)} className="h-fit w-fit">
+          <svg
+            className="rounded-full flex border bg-grey p-1.5 border-black shrink-0 ml-8"
+            width="30"
+            height="30"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              y="6"
+              width="30"
+              height="4"
+              rx="1"
+              className={`transform origin-center transition duration-200 ease-out ${
+                accordionOpen && "!rotate-180"
+              }`}
+            />
+            <rect
+              y="6"
+              width="30"
+              height="4"
+              rx="1"
+              className={`transform origin-center rotate-90 transition duration-200 ease-out ${
+                accordionOpen && "!rotate-180"
+              }`}
+            />
+          </svg>
+        </button>
+      </div>
+      <p
+        className={`grid overflow-hidden transition-all duration-300 ease-in-out text-sm  border-t-2 border-t-black ${
+          accordionOpen
+            ? "grid-rows-[1fr] opacity-100 pt-4 md:pt-7 mt-4 md:mt-7"
+            : "grid-rows-[0fr] opacity-0"
+        }`}
+      >
+        <div className="overflow-hidden">{content}</div>
+      </p>
+    </div>
+  );
+};
+
+export default Accordion;
--- src/js/data.js
+++ src/js/data.js
@@ -4,41 +4,68 @@ export const services = [
     title: "Search engine",
     color: "var(--green)",
     bgcolor: "var(--grey)",
-    learncolor: "var(--black)"
+    learncolor: "var(--black)",
   },
   {
     image: "/assets/images/service2.png",
     title: "Pay-per-click",
     color: "var(--white)",
     bgcolor: "var(--green)",
-    learncolor: "var(--black)"
+    learncolor: "var(--black)",
   },
   {
     image: "/assets/images/service3.png",
     title: "Social media",
     color: "var(--white)",
     bgcolor: "var(--dark)",
-    learncolor: "var(--white)"
+    learncolor: "var(--white)",
   },
   {
     image: "/assets/images/service4.png",
     title: "Email",
     color: "var(--green)",
     bgcolor: "var(--grey)",
-    learncolor: "var(--black)"
+    learncolor: "var(--black)",
   },
   {
     image: "/assets/images/service5.png",
     title: "Content Creation",
     color: "var(--white)",
     bgcolor: "var(--green)",
-    learncolor: "var(--black)"
+    learncolor: "var(--black)",
   },
   {
     image: "/assets/images/service6.png",
     title: "Analytics",
     color: "var(--green)",
     bgcolor: "var(--dark)",
-    learncolor: "var(--white)"
+    learncolor: "var(--white)",
   },
-]
+];
+
+export const accordion_elements = [
+  {
+    title: "Consultation",
+    content: "During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements."
+  },
+  {
+    title: "Research and Strategy Development",
+    content: "We'll conduct in-depth research to understand your target audience, analyze your competitors, and develop a winning marketing strategy tailored to your business goals."
+  },
+  {
+    title: "Implementation",
+    content: "Our team of experts will execute the agreed-upon marketing plan, ensuring smooth implementation across all channels."
+  },
+  {
+    title: "Monitoring",
+    content: "We'll closely monitor the performance of your marketing campaigns, providing real-time data and insights to optimize results."
+  },
+  {
+    title: "Reporting and Communication",
+    content: "Stay informed with regular reports and clear communication, keeping you up-to-date on campaign performance and progress."
+  },
+  {
+    title: "Continual Improvement",
+    content: "Through ongoing analysis and adjustments, we'll continuously refine your marketing strategy to maximize results."
+  }
+];
