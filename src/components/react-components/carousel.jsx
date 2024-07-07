c56479d428d71cd3c3a901e812390dadefc0ee84
Cheeshcake <mohamedazizhadjhassen@gmail.com>
2024-07-07 03:30:43 +0100

    carousel done
    this component is also done with react

---
+++ src/components/react-components/carousel.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { testimonials } from "../../js/data";
+
+const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+  };
+
+  const goToPrevious = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + testimonials.length) % testimonials.length
+    );
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center relative w-11/12 md:w-3/4 my-10 py-10 bg-dark overflow-hidden text-white m-auto rounded-3xl">
+      <div className="flex overflow-hidden w-full">
+        <div
+          className="flex transform duration-500 ease w-full"
+          style={{
+            transform: `translateX(-${currentIndex * 100}%)`,
+            transition: "transform 0.5s ease",
+          }}
+        >
+          {testimonials.map((testimonial, index) => (
+            <div
+              className={`p-5 w-full  ${index === currentIndex ? "active" : ""}`}
+              key={index}
+              style={{
+                flex: "0 0 100%",
+              }}
+            >
+              <div className="w-full md:w-3/5 m-auto">
+                <p className="border text-center border-green rounded-3xl p-3 md:p-8 w-full">
+                  {testimonial.text}
+                </p>
+                <div className="p-10">
+                  <h4 className="text-green">{testimonial.name}</h4>
+                  <p>{testimonial.title}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between items-center w-3/4 md:w-1/2 m-auto mt-4">
+        <button
+          className="rotate-180 bg-none border-none cursor-pointer text-4xl"
+          onClick={goToPrevious}
+        >
+          {"➔"}
+        </button>
+        <div className="flex justify-center mt-2">
+          {testimonials.map((_, index) => (
+            <span
+              key={index}
+              className={`indicator text-xl cursor-pointer my-1 ${
+                index === currentIndex ? "active" : ""
+              }`}
+              onClick={() => setCurrentIndex(index)}
+            >
+              ✦
+            </span>
+          ))}
+        </div>
+        <button
+          className="bg-none border-none cursor-pointer text-4xl"
+          onClick={goToNext}
+        >
+          {"➔"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/js/data.js
+++ src/js/data.js
@@ -108,3 +108,32 @@ export const members = [
     description: "2+ years of experience in writing and editing. Skilled in creating compelling, SEO-optimized content for various industries."
   }
 ];
+
+
+export const testimonials = [
+  {
+    text: "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+    name: "John Smith",
+    title: "Marketing Director at XYZ Corp",
+  },
+  {
+    text: "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+    name: "John Doe",
+    title: "Marketing Director at XYZ Corp",
+  },
+  {
+    text: "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+    name: "Michael Brown",
+    title: "Marketing Director at XYZ Corp",
+  },
+  {
+    text: "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+    name: "Michael Brown",
+    title: "Marketing Director at XYZ Corp",
+  },
+  {
+    text: "We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.",
+    name: "Michael Brown",
+    title: "Marketing Director at XYZ Corp",
+  },
+];
